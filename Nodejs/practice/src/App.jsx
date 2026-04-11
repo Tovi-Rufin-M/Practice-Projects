a7dbb3c321caf330e2c6b11a52950d522fee059d
2V_Rufin M. <rufintovi11@gmail.com>
2026-04-11 12:53:38 +0800

    edit

---
+++ Nodejs/practice/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <header>
+      <h1>hello, world!</h1>
+      
+    </header>
+    <p> 192.168.254.106</p>
+    </>
+  )
+}
+
+export default App
